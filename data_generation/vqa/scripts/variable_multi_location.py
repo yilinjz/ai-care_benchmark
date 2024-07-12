1031f6e0e21a18fbaaa36010dba88d199b6cdc6d
Jason Zhang <71229867+yilinjz@users.noreply.github.com>
2024-07-12 20:05:59 +0800

    checkpoint

--- data_generation/vqa/scripts/variable_multi_location.py
+++ data_generation/vqa/scripts/variable_multi_location.py
@@ -40,28 +40,284 @@ question_pool = [
 ### ========== VARIABLE ========== ###
 
 # ========== bathroom ==========
-scene_tag = "bathroom"
+# scene_tag = "bathroom"
+# frame_num = [44, 49, 170, 500, 505, 509, 656]
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
 
+# item_lists = [
+#     [['toilet', 'sink'], ['toothbrush', 'sink'], ['cup', 'toothbrush']],
+#     [['toilet', 'sink'], ['toothbrush', 'toothpaste'], ['bottle', 'toothbrush']],
+#     [['toilet', 'sink'], ['person', 'sink'], ['person', 'toilet']],
+#     [['toilet', 'sink'], ['toothbrush', 'sink'], ['person', 'sink']],
+#     [['toilet', 'sink'], ['bottle', 'sink'], ['bottle', 'toilet']],
+#     [['toothbrush', 'sink'], ['toothbrush', 'bottle'], ['sink', 'bottle']],
+#     [['toilet', 'sink'], ['bottle', 'toilet'], ['cup', 'sink']]
+# ]
 
-frame_num = [44, 49, 170, 500, 505, 509, 656]
+# ========== bedroom ========== #
+# scene_tag = "bedroom" # change
+# frame_num = [70, 176, 178, 181, 281, 401, 525, 946] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change 
+# item_lists = [
+#     [['vase', 'bed'], ['potted plant', 'vase'], ['dog', 'potted plant']],
+#     [['backpack', 'bed'], ['handbag', 'book'], ['book', 'bed']],
+#     [['chair', 'bed'], ['backpack', 'couch'], ['cup', 'chair']],
+#     [['keyboard', 'bed'], ['backpack', 'bed'], ['remote', 'tv']],
+#     [['teddy bear', 'bed'], ['cup', 'bed'], ['cup', 'teddy bear']],
+#     [['teddy bear', 'bed'], ['chair', 'bed']],
+#     [['laptop', 'bed'], ['backpack', 'bed'], ['bed', 'dining table']],
+#     [['tv', 'bed'], ['chair', 'bed'], ['mouse', 'book']]
+# ]
+
+# ========== dining room ========== #
+# scene_tag = "dining_room" # change
+# frame_num = [54, 746, 757, 783, 824, 852, 879, 880, 1436] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['laptop', 'dining table'], ['bottle', 'laptop'], ['microwave', 'bowl']],
+#     [['microwave', 'chair'], ['toaster', 'microwave'], ['bottle', 'toaster']],
+#     [['dining table', 'sink'], ['sink', 'tv'], ['cup', 'dining table']],
+#     [['bottle', 'backpack'], ['cup', 'backpack'], ['bottle', 'bowl']],
+#     [['dining table', 'refrigerator'], ['potted plant', 'dining table'], ['bowl', 'potted plant']],
+#     [['banana', 'dining table'], ['sink', 'banana'], ['bottle', 'sink']],
+#     [['tv', 'dining table'], ['potted plant', 'dining table'], ['tv', 'chair']],
+#     [['microwave', 'oven'], ['oven', 'dining table'], ['chair', 'dining table']],
+#     [['vase', 'dining table'], ['potted plant', 'dining table'], ['vase', 'chair']]
+# ]
+
+# ========== doorway ========== #
+# scene_tag = "doorway" # change
+# frame_num = [53, 251, 262, 263, 459, 460, 485, 562, 564] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['microwave', 'book'], ['bowl', 'microwave'], ['wine glass', 'book']],
+#     [['sink', 'oven'], ['refrigerator', 'oven'], ['couch', 'sink']],
+#     [['vase', 'tv'], ['book', 'tv'], ['tv', 'keyboard']],
+#     [['microwave', 'chair'], ['refrigerator', 'chair'], ['dining table', 'microwave']],
+#     [['potted plant', 'chair'], ['dining table', 'chair'], ['tv', 'dining table']],
+#     [['potted plant', 'tv'], ['chair', 'potted plant'], ['potted plant', 'vase']],
+#     [['tv', 'couch'], ['keyboard', 'tv'], ['book', 'couch']],
+#     [['microwave', 'person'], ['oven', 'refrigerator'], ['oven', 'microwave']],
+#     [['person', 'dog'], ['sink', 'person'], ['bowl', 'sink']]
+# ]
+
+# ========== kitchen ========== #
+# scene_tag = "kitchen" # change
+# frame_num = [138, 139, 199, 203, 204, 253, 560, 849] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['microwave', 'oven'], ['oven', 'bowl'], ['bowl', 'sink']],
+#     [['bowl', 'microwave'], ['knife', 'bottle'], ['bottle', 'oven']],
+#     [['knife', 'oven'], ['cup', 'oven'], ['bowl', 'oven']],
+#     [['oven', 'microwave'], ['sink', 'oven'], ['cup', 'sink']],
+#     [['toothbrush', 'sink'], ['wine glass', 'sink'], ['cup', 'wine glass']],
+#     [['microwave', 'sink'], ['sink', 'refrigerator'], ['bottle', 'oven']],
+#     [['sink', 'microwave'], ['oven', 'sink'], ['bottle', 'oven']],
+#     [['toaster', 'sink'], ['bowl', 'oven'], ['bowl', 'sink']]
+# ]
+
+# ========== living room ========== #
+# scene_tag = "living_room" # change
+# frame_num = [50, 156, 166, 200, 201, 261, 350, 356, 583, 1304] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['potted plant', 'couch'], ['book', 'couch'], ['book', 'potted plant']],
+#     [['tv', 'couch'], ['remote', 'couch'], ['remote', 'tv']],
+#     [['vase', 'dining table'], ['dining table', 'couch'], ['chair', 'couch']],
+#     [['bottle', 'dining table'], ['chair', 'dining table'], ['tv', 'dining table']],
+#     [['tv', 'dining table'], ['tv', 'book'], ['dining table', 'chair']],
+#     [['laptop', 'couch'], ['potted plant', 'couch'], ['bowl', 'couch']],
+#     [['bicycle', 'chair'], ['keyboard', 'chair'], ['potted plant', 'bicycle']],
+#     [['tv', 'potted plant'], ['book', 'tv'], ['potted plant', 'couch']],
+#     [['backpack', 'tv'], ['tv', 'couch'], ['remote', 'backpack']],
+#     [['vase', 'couch'], ['potted plant', 'chair'], ['book', 'vase']]
+# ]
+
+# ========== playroom ========== #
+scene_tag = "playroom" # change
+frame_num = [425, 426, 428, 437, 1081, 1084, 1157, 1203] # change
 scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
 
+# change
 item_lists = [
-    [['toilet', 'sink'], ['toothbrush', 'sink'], ['cup', 'toothbrush']],
-    [['toilet', 'sink'], ['toothbrush', 'toothpaste'], ['bottle', 'toothbrush']],
-    [['toilet', 'sink'], ['person', 'sink'], ['person', 'toilet']],
-    [['toilet', 'sink'], ['toothbrush', 'sink'], ['person', 'sink']],
-    [['toilet', 'sink'], ['bottle', 'sink'], ['bottle', 'toilet']],
-    [['toothbrush', 'sink'], ['toothbrush', 'bottle'], ['sink', 'bottle']],
-    [['toilet', 'sink'], ['bottle', 'toilet'], ['cup', 'sink']]
+    [['sports ball', 'chair'], ['backpack', 'sports ball'], ['backpack', 'dining table']],
+    [['tv', 'couch'], ['couch', 'chair'], ['sports ball', 'tv']],
+    [['suitcase', 'couch'], ['chair', 'couch'], ['couch', 'dining table']],
+    [['tv', 'chair'], ['teddy bear', 'tv'], ['chair', 'dog']],
+    [['chair', 'bed'], ['book', 'bed'], ['book', 'chair']],
+    [['tv', 'chair'], ['chair', 'book'], ['remote', 'tv']],
+    [['teddy bear', 'bed'], ['book', 'backpack'], ['teddy bear', 'book']],
+    [['backpack', 'couch'], ['suitcase', 'couch'], ['backpack', 'chair']]
 ]
 
-# answer_keys = [
-#     ['down and left', 'top/top and left', 'right'],
-#     ['down and left', 'left', 'left'],
-#     ['down and left', 'up', 'up and right'],
-#     ['down and right', 'left', 'up'],
-#     ['left/down and left', 'up/up and left/up and right', 'up and right'],
-#     ['up', 'up/up and right', 'right'],
-#     ['down and left', 'up', 'up/up and left']
+# ========== lobby ========== #
+# scene_tag = "lobby" # change
+# frame_num = [9, 36, 619, 625, 1251, 1253] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== meeting room ========== #
+# scene_tag = "meeting_room" # change
+# frame_num = [5, 27, 39, 341] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== pantry ========== #
+# scene_tag = "pantry" # change
+# frame_num = [1, 410, 412, 415, 417] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== workstation ========== #
+# scene_tag = "workstation" # change
+# frame_num = [4, 23, 371, 388, 393, 453, 455, 474] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== bookstore ========== #
+# scene_tag = "bookstore" # change
+# frame_num = [88, 89, 91, 112, 119] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== classroom ========== #
+# scene_tag = "classroom" # change
+# frame_num = [297, 301, 323, 325, 326] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== coffee shop ========== #
+# scene_tag = "coffee_shop" # change
+# frame_num = [120, 122, 123, 223, 225, 228, 238, 244] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== computer lab ========== #
+# scene_tag = "computer lab" # change
+# frame_num = [272, 279, 333, 335, 337, 338] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
+# ]
+
+# ========== dorm ========== #
+# scene_tag = "dorm" # change
+# frame_num = [128, 130, 131, 143, 267, 569] # change
+# scene_ids = [f"{scene_tag}_{x}" for x in frame_num]
+
+# # change
+# item_lists = [
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']],
+#     [['', ''], ['', ''], ['', '']]
 # ]
