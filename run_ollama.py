4e1d7986551160f18f3975d57f771ba128db1b43
Jason Zhang <71229867+yilinjz@users.noreply.github.com>
2024-07-22 20:04:33 +0800

    Update run_ollama.py

--- run_ollama.py
+++ run_ollama.py
@@ -71,7 +71,7 @@ def cmd_agent():
         # print(path_to_instruction)
 
         model_name = model_id.replace(':', '_')
-        with open(f'benchmark/experiment_result/{model_name}-{benchmark_name}-experiment_result.json', 'w', encoding='utf-8') as f:
+        with open(f'benchmark/experiment_result/{model_name}/{model_name}-{benchmark_name}-experiment_result.json', 'w', encoding='utf-8') as f:
                 json.dump(data, f, ensure_ascii=False, indent=4)                 
 
     log.info("SYSTEM: cmd agent end <<<<<")
